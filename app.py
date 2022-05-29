1850e702fe2af850e03186a36692cd2cee318f0a
Peter Ha <peterha2023@u.northwestern.edu>
2022-05-29 06:22:52 -0500

    updated app.py

--- app.py
+++ app.py
@@ -23,10 +23,11 @@ menu = api.populate_meal_period(data, "Breakfast")
 dining_hall = "Allison"
 
 # returns a cursor object that must be iterated to obtain a dict
-foods_all_meals = mdb.collection.find({'$and':[{'dining_hall': dining_hall}, {'date': mdb.date}]})
+foods_all_meals = mdb.collection.find({'$and': [{'dining_hall': dining_hall}, {'date': mdb.date}]})
 
 for food_items in foods_all_meals:
     foods_dict = food_items
+
 
 def get_leftover(foods_at_meal):
     '''
@@ -41,22 +42,30 @@ def get_leftover(foods_at_meal):
     # iterate through each category and the food items in each category
     for category in foods_at_meal:
         for food_dict in list(category.values())[0]:
-                if food_dict["leftover"] == "yes":
-                    leftover_foods.append(food_dict["name"])
+            if food_dict["leftover"] == "yes":
+                leftover_foods.append(food_dict["name"])
 
     return leftover_foods
-    
 
+dining_hall = ""
 
 ##############
 # Exercise 1 #
 ##############
-@app.route('/')
-@app.route('/home.html')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home.html', methods=['GET', 'POST'])
 def main_page():
+    global dining_hall
+    if request.method == 'POST':
+        dining_hall = request.form.get('dining')
+        time = request.form.get('meal')
+        print(dining_hall, time)
+        return render_template(time)
+
     return render_template("home.html")
 
-@app.route('/breakfast.html', methods = ['GET', 'POST'])
+
+@app.route('/breakfast.html', methods=['GET', 'POST'])
 def breakfast():
     foods = foods_dict["Breakfast"]
     is_leftover = get_leftover(foods)
@@ -68,20 +77,21 @@ def breakfast():
                 yes_to_no.append(food)
             else:
                 no_to_yes.append(food)
-            update_mongo(yes_to_no, no_to_yes)
+            update_mongo(dining_hall, yes_to_no, no_to_yes)
         return render_template("finish.html")
 
     length = len(foods)
-    firstfoods = foods[:round(length/3)]
-    secondfoods = foods[round(length/3):round(2 * length/3)]
-    thirdfoods = foods[round(2 * length/3):round(length/3)]
+    firstfoods = foods[:round(length / 3)]
+    secondfoods = foods[round(length / 3):round(2 * length / 3)]
+    thirdfoods = foods[round(2 * length / 3):round(length / 3)]
 
     return render_template("breakfast.html",
                            firstfoods=firstfoods,
                            secondfoods=secondfoods,
                            thirdfoods=thirdfoods)
 
-@app.route('/lunch.html', methods = ['GET', 'POST'])
+
+@app.route('/lunch.html', methods=['GET', 'POST'])
 def lunch():
     foods = foods_dict["Lunch"]
     is_leftover = get_leftover(foods)
@@ -93,7 +103,7 @@ def lunch():
                 yes_to_no.append(food)
             else:
                 no_to_yes.append(food)
-            update_mongo(yes_to_no, no_to_yes)
+            update_mongo(dining_hall, yes_to_no, no_to_yes)
         return render_template("finish.html")
 
     ### REPLACE ###
@@ -101,16 +111,17 @@ def lunch():
     # foods = menu["Lunch"][0]
     ### REPLACE ###
     length = len(foods)
-    firstfoods = foods[:round(length/3)]
-    secondfoods = foods[round(length/3):round(2 * length/3)]
-    thirdfoods = foods[round(2 * length/3):round(length/3)]
+    firstfoods = foods[:round(length / 3)]
+    secondfoods = foods[round(length / 3):round(2 * length / 3)]
+    thirdfoods = foods[round(2 * length / 3):round(length / 3)]
 
     return render_template("lunch.html",
                            firstfoods=firstfoods,
                            secondfoods=secondfoods,
                            thirdfoods=thirdfoods)
 
-@app.route('/dinner.html', methods = ['GET', 'POST'])
+
+@app.route('/dinner.html', methods=['GET', 'POST'])
 def dinner():
     foods = foods_dict["Dinner"]
     is_leftover = get_leftover(foods)
@@ -122,44 +133,50 @@ def dinner():
                 yes_to_no.append(food)
             else:
                 no_to_yes.append(food)
-            update_mongo(yes_to_no, no_to_yes)
+            update_mongo(dining_hall, yes_to_no, no_to_yes)
         return render_template("finish.html")
 
     ### REPLACE ###
     # menu = api.populate_meal_period(data, "Dinner")
     # foods = menu["Dinner"][0]
-    
+
     ### REPLACE ###
 
     length = len(foods)
-    firstfoods = foods[:round(length/3)]
-    secondfoods = foods[round(length/3):round(2 * length/3)]
-    thirdfoods = foods[round(2 * length/3):round(length/3)]
+    firstfoods = foods[:round(length / 3)]
+    secondfoods = foods[round(length / 3):round(2 * length / 3)]
+    thirdfoods = foods[round(2 * length / 3):round(length / 3)]
 
     return render_template("dinner.html",
                            firstfoods=firstfoods,
                            secondfoods=secondfoods,
                            thirdfoods=thirdfoods)
 
+
 @app.route('/index.html')
 def index():
     return render_template("index.html")
+
 
 @app.route('/finish.html')
 def finish():
     return render_template("finish.html")
 
+
 @app.route('/s_breakfast.html')
 def s_breakfast():
     return render_template("s_breakfast.html")
+
 
 @app.route('/s_dinner.html')
 def s_dinner():
     return render_template("s_dinner.html")
 
+
 @app.route('/s_home.html')
 def s_home():
     return render_template("s_home.html")
+
 
 @app.route('/s_lunch.html')
 def s_lunch():
@@ -178,11 +195,11 @@ def update_mongo(dining_hall, yes_to_no, no_to_yes):
     for food_entry in new_leftover:
         for meal in ["Breakfast", "Lunch", "Dinner"]:
             category_index = 0
-            for category in dining_hall[meal]: # iterate through Comfort, Rooted, etc
+            for category in foods_dict[meal]: # iterate through Comfort, Rooted, etc
                 category_name = list(category.keys())[0]
                 food_index = 0
-                for food_dict in list(category.values())[0]:
-                    if food_dict["name"] == food_entry:
+                for food_item_dict in list(category.values())[0]:
+                    if food_item_dict["name"] == food_entry:
                         found = True
                         food_meal = meal
                     if not found:
@@ -204,11 +221,11 @@ def update_mongo(dining_hall, yes_to_no, no_to_yes):
     for food_entry in new_not_leftover:
         for meal in ["Breakfast", "Lunch", "Dinner"]:
             category_index = 0
-            for category in dining_hall[meal]: # iterate through Comfort, Rooted, etc
+            for category in foods_dict[meal]: # iterate through Comfort, Rooted, etc
                 category_name = list(category.keys())[0]
                 food_index = 0
-                for food_dict in list(category.values())[0]:
-                    if food_dict["name"] == food_entry:
+                for food_item_dict in list(category.values())[0]:
+                    if food_item_dict["name"] == food_entry:
                         found = True
                         food_meal = meal
                     if not found:
